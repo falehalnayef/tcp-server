efc37a8f39cc0460323144c842a515a869f0f384
falehalnayef <alnayef.faleh@gmail.com>
2025-01-18 20:26:23 +0300

    feat: set up the tcp server.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
+use std::{io::Write, net::TcpListener};
 
 fn main() {
     println!("http server!");
-    
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    println!("Listening on {}", listener.local_addr().unwrap());
 
+    for stream in listener.incoming(){
+
+        println!("Connection established!");
+
+        match stream {
+            Ok(mut stream) =>{
+                stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+
+            },
+            Err(err) => {
+
+                eprintln!("{}", err);
+            }
+        }
+    }
 }
